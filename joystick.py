e7b01e90d3b8b0b8e57a35687587e7a8fe464971
Yujie Wen <yjwen.ty@qq.com>
2018-07-08 08:37:16 +0000

    Moving robot

--- joystick.py
+++ joystick.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import time
 import thread
+import easygopigo3 as easy
 
 class JoyStatus:
     def __init__(self):
@@ -9,12 +10,26 @@ class JoyStatus:
         self.accelerate = False
         self.bark = False
 
-def control_gopigo(joystatus):
+def control_gopigo(robot, joystatus):
     while True:
-        time.sleep(0.5)
-        if joystatus.direction != (0, 0):
-            print "GoPiGo: Moving to {}".format(joystatus.direction)
+        d = joystatus.direction
+        if d != (0, 0):
+            if d[1] != 0:
+                print "Driving robot {}".format(d[1])
+                robot.drive_degrees(d[1] * 10)
+                print "Done"
+            elif d[0] != 0:
+                print "Turning robot {}".format(d[0])
+                robot.turn_degrees(d[0] * 5)
+                print "Done"
+        else:
+            time.sleep(0.5)
 
+
+robot = easy.EasyGoPiGo3()
+robot.close_eyes()
+robot.set_left_eye_color((255, 0, 0))
+robot.open_left_eye()
 pygame.init()
 
 pygame.joystick.init()
@@ -25,24 +40,19 @@ if (js_count == 0):
     sys.exit()
 joystatus = JoyStatus()
 
-thread.start_new_thread(control_gopigo, (joystatus,))
+thread.start_new_thread(control_gopigo, (robot, joystatus))
 
 clock = pygame.time.Clock()
 count = 0
+robot.set_right_eye_color((255, 0, 0))
+robot.open_right_eye()
 while (True):
     clock.tick(1)
     print "Count={}".format(count)
     pygame.event.get()
     js = pygame.joystick.Joystick(0)
     js.init()
-    for i in range(js.get_numaxes()):
-        axis = js.get_axis(i)
-        print("Axis {} value: {:>6.3f}".format(i, axis))
-    for i in range(js.get_numbuttons()):
-        button = js.get_button(i)
-        print "Button {:>2} value: {}".format(i, button)
     joystatus.direction = js.get_hat(0)
-    print joystatus.direction
     count = count + 1
 
 sys.exit()
